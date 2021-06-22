89ce39d192f6f940d30cbdf6552d4e774a50b398
田剑 <1468195034@qq.com>
2021-06-22 10:20:13 +0800

    缓存逻辑添加

--- src/main/java/fulan/tianjian/demo/model/web/ResponseValue.java
+++ src/main/java/fulan/tianjian/demo/model/web/ResponseValue.java
@@ -25,12 +25,41 @@ public class ResponseValue<T> {
         this.code = code;
     }
 
-    public static final ResponseValue failResponse() {
-        return new ResponseValue("返回结果错误", REST_COMMON_FAIL_CODE);
+    public static<T> ResponseValue<T> failResponse() {
+        return new ResponseValue<T>("返回结果错误", REST_COMMON_FAIL_CODE);
     }
 
     public static<T> ResponseValue<T> successResponse(T data) {
-        ResponseValue responseValue = new ResponseValue("成功", REST_COMMON_SUCCESS_CODE);
+        ResponseValue<T> responseValue = new ResponseValue<T>("成功", REST_COMMON_SUCCESS_CODE, data);
         return responseValue;
     }
+
+	public static ResponseValue<String> failResponse(String message) {
+		return new ResponseValue<String>(message, REST_COMMON_FAIL_CODE);
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+	
 }
